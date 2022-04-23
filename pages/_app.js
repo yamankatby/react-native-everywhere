42f2df5a75d2646204d686f74d8e66c5a49a0796
Yaman KATBY <m.yaman.katby@gmail.com>
2022-04-23 19:05:44 +0300

    Work on the Sidebar component

--- pages/_app.js
+++ pages/_app.js
@@ -10,50 +10,52 @@ const index = [
     { title: 'Browser Extension', href: '/browser-extension' },
     { title: 'Windows and macOS', href: '/desktop' },
     { title: 'Electron', href: '/electron' },
-    { title: 'Menubar App', href: '/menubar-app' }
+    { title: 'Menubar App', href: '/menubar-app' },
   ],
   [
     { title: 'Building for large screens', href: '/building-for-large-screens' },
     { title: 'Overview', href: '/' },
     { title: 'Responsive Design', href: '/responsive-design' },
-    { title: 'Layout', href: '/layout' }
+    { title: 'Layout', href: '/layout' },
   ],
   [
     { title: 'Studies', href: '/studies' },
     { title: 'Overview', href: '/' },
     { title: 'Habit Tracker', href: '/habit-tracker' },
-    { title: 'Pomodoro - Time Tracker', href: '/pomodoro' }
+    { title: 'Pomodoro - Time Tracker', href: '/pomodoro' },
   ],
-  { title: 'Acknowledgments', href: '/acknowledgments' }
+  { title: 'Acknowledgments', href: '/acknowledgments' },
 ];
 
 const SidebarItem = ({ title, href }) => (
-  <Link href={href}>
-    <a>{title}</a>
-  </Link>
+  <li>
+    <Link href={href}>
+      <a>{title}</a>
+    </Link>
+  </li>
 );
 
 const SidebarGroup = ({ items }) => (
-  <>
+  <li>
     <h3>{items[0].title}</h3>
     <ul>
       {items.slice(1).map((item) => (
-        <li key={item.href}>
-          <SidebarItem title={item.title} href={items[0].href + item.href} />
-        </li>
+        <SidebarItem key={item.href} title={item.title} href={items[0].href + item.href} />
       ))}
     </ul>
-  </>
+  </li>
 );
 
 const Sidebar = () => (
   <aside>
     <ul>
-      {index.map((item, index) => (
-        <li key={index}>
-          {Array.isArray(item) ? <SidebarGroup items={item} /> : <SidebarItem title={item.title} href={item.href} />}
-        </li>
-      ))}
+      {index.map((item) =>
+        Array.isArray(item) ? (
+          <SidebarGroup key={item[0].href} items={item} />
+        ) : (
+          <SidebarItem key={item.href} title={item.title} href={item.href} />
+        )
+      )}
     </ul>
   </aside>
 );
