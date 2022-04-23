2157676c142b23a29a636f79522a156ba18af61c
Yaman KATBY <m.yaman.katby@gmail.com>
2022-04-23 18:56:23 +0300

    Add Sidebar component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,68 @@
+import Link from 'next/link';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+const index = [
+  { title: 'Overview', href: '/' },
+  [
+    { title: 'Running React Native everywhere', href: '/running-react-native-everywhere' },
+    { title: 'Mobile (Android and iOS)', href: '/mobile' },
+    { title: 'Web', href: '/web' },
+    { title: 'Browser Extension', href: '/browser-extension' },
+    { title: 'Windows and macOS', href: '/desktop' },
+    { title: 'Electron', href: '/electron' },
+    { title: 'Menubar App', href: '/menubar-app' }
+  ],
+  [
+    { title: 'Building for large screens', href: '/building-for-large-screens' },
+    { title: 'Overview', href: '/' },
+    { title: 'Responsive Design', href: '/responsive-design' },
+    { title: 'Layout', href: '/layout' }
+  ],
+  [
+    { title: 'Studies', href: '/studies' },
+    { title: 'Overview', href: '/' },
+    { title: 'Habit Tracker', href: '/habit-tracker' },
+    { title: 'Pomodoro - Time Tracker', href: '/pomodoro' }
+  ],
+  { title: 'Acknowledgments', href: '/acknowledgments' }
+];
 
-export default MyApp;
+const SidebarItem = ({ title, href }) => (
+  <Link href={href}>
+    <a>{title}</a>
+  </Link>
+);
+
+const SidebarGroup = ({ items }) => (
+  <>
+    <h3>{items[0].title}</h3>
+    <ul>
+      {items.slice(1).map((item) => (
+        <li key={item.href}>
+          <SidebarItem title={item.title} href={items[0].href + item.href} />
+        </li>
+      ))}
+    </ul>
+  </>
+);
+
+const Sidebar = () => (
+  <aside>
+    <ul>
+      {index.map((item, index) => (
+        <li key={index}>
+          {Array.isArray(item) ? <SidebarGroup items={item} /> : <SidebarItem title={item.title} href={item.href} />}
+        </li>
+      ))}
+    </ul>
+  </aside>
+);
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <div>
+      <Sidebar />
+      <Component {...pageProps} />
+    </div>
+  );
+}
