16142250f7246251079bdb309d6a60caf3c67c85
Yaman KATBY <m.yaman.katby@gmail.com>
2022-05-14 19:05:23 +0300

    Work on the Post page

--- pages/[[...slug]].js
+++ pages/[[...slug]].js
@@ -1,9 +1,9 @@
 import fs from "fs/promises";
 import path from "path";
-import { globby } from "globby"
-import matter from 'gray-matter'
-import { serialize } from 'next-mdx-remote/serialize'
-import { MDXRemote } from 'next-mdx-remote'
+import { globby } from "globby";
+import matter from "gray-matter";
+import { serialize } from "next-mdx-remote/serialize";
+import { MDXRemote } from "next-mdx-remote";
 
 export async function getStaticPaths() {
   const paths = await globby(["posts/**/*"]);
@@ -16,19 +16,27 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps(context) {
-  const paths = await globby(["posts/**/*"]);
+  const paths = await globby(["posts"]);
+
   const allPosts = [];
-  for (let p of paths) {
-    const file = await fs.readFile(path.join(process.cwd(), p), 'utf8')
-    const {data, content} = matter(file)
-    const mdxSource = await serialize(content)
-    allPosts.push({data, content: mdxSource})
+  for (const p of paths) {
+    const file = await fs.readFile(path.join(process.cwd(), p), "utf8");
+    const { data: frontmatter, content } = matter(file);
+    const source = await serialize(content);
+    allPosts.push({
+      slug: p.match(/posts\/(.*?)(\/?index)?.md/)[1],
+      frontmatter,
+      source
+    });
   }
-  return { props:  allPosts[1] }
+
+  const slug = context.params.slug?.join("/") ?? "";
+  const currentIndex = allPosts.findIndex((post) => post.slug === slug);
+  const current = allPosts[currentIndex];
+
+  return { props: { source: current.source } };
 }
 
 export default function Post(props) {
-  return (
-    <MDXRemote {...props.content}/>
-  )
+  return <MDXRemote {...props.source} />;
 }
